6b3280eb93840f126931cd93a27321604295e260
Kirill Ivanov <hoprosx@gmail.com>
2023-08-28 13:56:48 +0500

    feat: add GuessingGame class functional

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.average;
     }
 
     guess() {
-
+        this.average = Math.ceil((this.max - this.min) / 2 + this.min);
+        return this.average;
     }
 
     lower() {
-
+        this.setRange(this.min, this.average);
     }
 
     greater() {
-
+        this.setRange(this.average, this.max);
     }
 }
 
